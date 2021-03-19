41885f6d968223e6716ce9b51d6c9c346e0f11b3
MasterJhonny <jhonnyeddy.22@gmail.com>
2021-03-18 23:30:22 -0400

    se añadio el dia en la entrada de datos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 (function(){
-    var dias = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+    var dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
     var salidaHora = document.getElementById("salidaHora");
     var salidaDia = document.getElementById("salidaDia");
     var boton = document.getElementById("boton");
     var sonido = document.getElementById("sonido");
     var pantalla = document.getElementById("pantalla");
-    var entrada;
+    var entrada, day, dayWeek;
     var alarmas = [];
     var bolean = true;
     var horaActual, url;
@@ -33,7 +33,11 @@
         minuto = demo(minuto);
         hora = demo(hora);
         salidaHora.innerText = `${hora}:${minuto}:${segundo}`;
-        salidaDia.innerText = `${dias[dia]} ${diaSemana}`;
+        if(dia == 0){
+            salidaDia.innerText = `${dias[6]} ${diaSemana}`;
+        } else {
+            salidaDia.innerText = `${dias[dia-1]} ${diaSemana}`;
+        }
     }
     function demo(item){
         item = item.toString();
@@ -48,14 +52,26 @@
         return fana1 + fana2;
     }
     function crearAlarma(){
+        day = document.getElementById("day");
+        day = sacarDia(day.value);
+        diaWeek();
         entrada = document.getElementById("entrada");
         url = document.getElementById("url")
         alarmas.push(new Alarma(entrada.value, url.value));
         pantalla.innerText += `La Alarma Sonara a las ${entrada.value}\n`; 
-        console.log(`La Alarma Sonara a las ${entrada.value}`);
+        console.log(`La Alarma Sonara el dia ${dayWeek} ${day} a las ${entrada.value}`);
     }
     function abrirVinculo (URL){ 
         window.open(URL,"ventana1","menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes"); 
+    }
+    function sacarDia(fecha){
+        fecha = fecha.substring(8);
+        return fecha;
+    }
+    function diaWeek(){
+        var dia = document.getElementById("day");
+        let date = new Date(dia.value);
+        dayWeek = dias[date.getDay()];
     }
     actualizarHora();
     var intervalo = setInterval(actualizarHora, 999);
@@ -63,7 +79,3 @@
     
     
 }())
-
-
-
-
