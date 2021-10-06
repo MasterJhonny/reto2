f231d0dddee46a4ae61a4f20800e6824f4846c06
MasterJhonny <jhonnyeddy.22@gmail.com>
2021-10-06 01:43:40 -0400

    se anadio el menu desplegable

--- js/sentigs.js
+++ js/sentigs.js
@@ -1,6 +1,10 @@
 let doc = document.querySelector("html");
+// btns is element
 let btnMode = document.getElementById("btnMode");
+let btnMenu = document.getElementById("btnMenu");
 let hora = document.getElementById("salidaHora");
+
+// event handlers od btn modde darks
 btnMode.onclick = () => {
     doc.classList.toggle('dark-mode');
     let image = document.querySelector("iframe");
@@ -14,3 +18,14 @@ btnMode.onclick = () => {
         hora.style.color = "#212121";
     }
 } 
+
+// event handlers of btn menu
+btnMenu.onclick = () => {
+    let nav = document.querySelector(".nav");
+    nav.classList.toggle("flex");
+    if(nav.classList.contains("flex")){
+        btnMenu.style.backgroundImage = "url(https://cdn-icons-png.flaticon.com/512/2919/2919543.png)";
+    } else {
+        btnMenu.style.backgroundImage = "url(https://cdn-icons-png.flaticon.com/512/2099/2099153.png)";
+    }
+}
